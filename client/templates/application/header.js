85b2df69eb7c69e80a83efb4ed5eceea09c823ae
flavien <flavien.pensato@gmail.com>
2015-01-08 13:54:59 +0100

    Adding package mizzao:user-status (online/offline)

--- client/templates/application/header.js
+++ client/templates/application/header.js
@@ -2,7 +2,7 @@ Template.header.events({
   'click .new': function () {
     bootbox.prompt("Spreadsheet name", function(result) {                
       if (result) {                                             
-        Spreadsheets.insert({'name': result});
+        Spreadsheets.insert({'name': result, 'userId': Meteor.userId()});
       }
     });
   }
--- client/templates/items/spreadsheets/spreadsheets.js
+++ client/templates/items/spreadsheets/spreadsheets.js
@@ -2,7 +2,7 @@ Template.spreadsheets.events({
   'click .new': function () {
     bootbox.prompt("Spreadsheet name", function(result) {                
       if (result) {                                             
-        Spreadsheets.insert({'name': result});
+        Spreadsheets.insert({'name': result, 'userId': Meteor.userId()});
       }
     });
   }
