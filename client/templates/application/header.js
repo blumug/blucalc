ee204b23f4dba1f3e1404537a33ee3539bbebb7f
flavien <flavien.pensato@gmail.com>
2015-01-08 11:30:06 +0100

    Fix css to list

---
+++ client/templates/application/header.js
@@ -0,0 +1,9 @@
+Template.header.events({
+  'click .new': function () {
+    bootbox.prompt("Spreadsheet name", function(result) {                
+      if (result) {                                             
+        Spreadsheets.insert({'name': result});
+      }
+    });
+  }
+});
