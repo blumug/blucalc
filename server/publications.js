3a150a64bcb6dc56b789e0ae59fce35aefa1416c
flavien <flavien.pensato@gmail.com>
2015-01-09 18:04:41 +0100

    Refactor server

--- server/publications.js
+++ server/publications.js
@@ -25,10 +25,12 @@ Meteor.setInterval(function() {
   var now = (new Date()).getTime();
 
   if (Spreadsheets.find().count() > 0) {
-    var spreadsheet = Spreadsheets.find().fetch()[0];
-    for (var i = spreadsheet.usersOnline.length - 1; i >= 0; i--) {
-      if (now - 10 * 1000 < spreadsheet.usersOnline[i].now) {
-        Meteor.call("isNotalive", spreadsheet._id);
+    var spreadsheet = Spreadsheets.find().fetch();
+    for (var i = spreadsheet.length - 1; i >= 0; i--) {
+      for (var x = spreadsheet[i].usersOnline.length - 1; x >= 0; i--) {
+        if (now - 10 * 1000 < spreadsheet[i].usersOnline[x].now) {
+          Meteor.call("isNotalive", spreadsheet[i]._id);
+        }
       }
     }
   }
