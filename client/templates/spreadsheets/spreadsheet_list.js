cd342f485d9bc168ba1fdca7002af2b7420bdefa
Jérôme Blondon <jerome@blondon.fr>
2015-01-10 08:59:24 +0100

    escape filter query string before querying database

--- client/templates/spreadsheets/spreadsheet_list.js
+++ client/templates/spreadsheets/spreadsheet_list.js
@@ -1,10 +1,16 @@
+
+
 Template.spreadsheetList.helpers({
   'spreadsheetList': function() {
     var filter = Session.get("searchBar");
+    var escape = function(text) {
+      return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+    };
 
     if (filter && filter != "") {
+      filter = escape(filter);
+
       return Spreadsheets.find({
-        userId: Meteor.userId(),
         name: {
           $regex: filter,
           $options: "i"
